7d8cbc3e887dfb4a448fbcf4c08a4e809adcbff9
eagboola <eagboola@hawk..iit.edu>
2017-02-21 08:32:13 -0600

    2/20 changes
    orbit

---
+++ Assets/scripts/sinTransformation.cs
@@ -0,0 +1,25 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sinTransformation : MonoBehaviour
+{
+    Vector3 _startPosition;
+    public float domain;
+    public float scalar;
+
+
+    //use this for initialization
+    private void Start()
+    {
+        _startPosition = transform.position;
+        domain = Random.Range(0.5f, 50f);
+        scalar = Random.Range(0.5f, 50f);
+    }
+    
+
+    private void Update()
+    {
+        transform.position = _startPosition + new Vector3((Mathf.Sin(Time.unscaledTime * 5.0f) * scalar),  0.0f, 0.0f);
+    }
+ }
---
+++ Assets/scripts/verticalTransformation.cs
@@ -0,0 +1,25 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verticalTransformation : MonoBehaviour
+{
+    Vector3 _startPosition;
+    public float domain;
+    public float scalar;
+
+
+    //use this for initialization
+    private void Start()
+    {
+        _startPosition = transform.position;
+        domain = Random.Range(10f, 50f);
+        scalar = Random.Range(10f, 50f);
+    }
+
+    private void Update()
+    {
+		transform.position = _startPosition + new Vector3((Mathf.Sin(Time.unscaledTime * 5.0f) * scalar), (Mathf.Sin(Time.unscaledTime * 5.0f) * scalar), (Mathf.Sin(Time.unscaledTime * 5.0f) * scalar));
+
+    }
+}
